0d49b4a9b2754662036076c09e1134e92fb48686
Felipe Gueller <felipe.gueller@gmail.com>
2023-06-09 15:24:56 -0300

    :sparkles: feat: adding unidade column

---
+++ Migrations/20230609175457_adding-column-unidade.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AluraLoja.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingcolumnunidade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Preco",
+                table: "Produto",
+                newName: "PrecoUnitario");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Unidade",
+                table: "Produto",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unidade",
+                table: "Produto");
+
+            migrationBuilder.RenameColumn(
+                name: "PrecoUnitario",
+                table: "Produto",
+                newName: "Preco");
+        }
+    }
+}
