a0130b4447a44bf22a6397bd0b2df78f1bc61e8e
Felipe Gueller <felipe.gueller@gmail.com>
2023-06-12 17:13:09 -0300

    :recycle: style: adding the DAO classes and interfaces on DAO directory

---
+++ DAOs/ProdutoDAOEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraLoja.DAOs
+{
+    internal class ProdutoDAOEntity : IProdutoDAO, IDisposable
+    {
+        private readonly LojaContexto contexto;
+
+        public ProdutoDAOEntity()
+        {
+            contexto = new();
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            contexto.Add(produto);
+            contexto.SaveChanges();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            contexto.Update(produto);
+            contexto.SaveChanges();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            return contexto.Produtos.ToList();
+        }
+
+        public void Remover(Produto produto)
+        {
+            contexto.Remove(produto);
+            contexto.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+    }
+}
